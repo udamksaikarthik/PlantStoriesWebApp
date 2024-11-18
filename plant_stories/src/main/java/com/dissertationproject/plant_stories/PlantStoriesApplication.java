b7c427cbea812c9a719a11897ef825d06c1b4531
udamksaikarthik <50781377+udamksaikarthik@users.noreply.github.com>
2024-11-18 19:12:54 +0000

    First Commit

---
+++ plant_stories/src/main/java/com/dissertationproject/plant_stories/PlantStoriesApplication.java
@@ -0,0 +1,13 @@
+package com.dissertationproject.plant_stories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantStoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantStoriesApplication.class, args);
+	}
+
+}
---
+++ plant_stories/src/test/java/com/dissertationproject/plant_stories/PlantStoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dissertationproject.plant_stories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantStoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
