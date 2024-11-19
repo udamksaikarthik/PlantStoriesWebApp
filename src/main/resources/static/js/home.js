03f26965504bed5bc76401b5bdf23dfce4d0abab
udamksaikarthik <50781377+udamksaikarthik@users.noreply.github.com>
2024-11-19 15:53:33 +0000

    Frontend styles changes on home dashboard

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -1,6 +1,6 @@
 const mobile_menu_img_id = document.getElementById('mobile_menu_img_id')
 const mobile_menu_container_id = document.getElementById('mobile_menu_container_id')
-
+const mobile_greeding_container_id = document.getElementById('mobile-left-alignment-header-container-id')
 function menuBtn(){
 	console.log('Inside menuBtn function')
 	
@@ -9,8 +9,10 @@ function menuBtn(){
 	if(mobile_menu_img_id.src === absoluteUrl){
 		mobile_menu_img_id.src = "/images/close.png"
 		mobile_menu_container_id.style.display = "flex"
+		mobile_greeding_container_id.style.marginTop = "5%"
 	}else{
 		mobile_menu_img_id.src = "/images/mobile_menu_icon.png"
 		mobile_menu_container_id.style.display = "none"
+		mobile_greeding_container_id.style.marginTop = "40%"
 	}
 }
